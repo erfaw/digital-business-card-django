d033c09ef3dfef0e672147e7712b58c97a5471fd
erfaw <erfawn.h@gmail.com>
2026-07-29 00:51:11 +0330

    fix: remove pages/templates from TEMPLATES[0]['DIRS']
    -was wrong and unnecessary because APP_DIRS is True.

--- core/settings/base.py
+++ core/settings/base.py
@@ -31,7 +31,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR/'pages'/'templates'],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
