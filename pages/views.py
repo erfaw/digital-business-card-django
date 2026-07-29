6cf553318d764f9475b88433e84842708ca5873b
erfaw <erfawn.h@gmail.com>
2026-07-29 04:57:25 +0330

    feat: get data input and make record

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pages.models import OwnerInfoModel
 
 def index(request):
     """
@@ -10,4 +11,23 @@ def dashboard(request):
     """
     Render dashboard to manipulate details of card.
     """
+    if request.method == "POST":
+        new_owner = OwnerInfoModel.objects.create(
+            name=request.POST['name'],
+            role=request.POST['role'],
+            description=request.POST['description'],
+            logo_sub=request.POST['logo_sub'],
+            tag_behind=request.POST['tag_behind'],
+            email=request.POST['email'],
+            mobile_number=request.POST['mobile_number'],
+            website=request.POST['website'],
+            website_preview=request.POST['website_preview'],
+            linkedin=request.POST['linkedin'],
+            linkedin_preview=request.POST['linkedin_preview'],
+            location=request.POST['location'],
+            response_time=request.POST['response_time'],
+        )
+        new_owner.save()
+        return redirect('index')
+
     return render(request, "dashboard.html")
