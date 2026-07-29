b6974472ab8eac0695b1dae8833943b313810680
erfaw <erfawn.h@gmail.com>
2026-07-30 02:44:25 +0330

    feat: register OwnerInfoModel and Contact to admin.site
    -make a ContactAdmin class too

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import OwnerInfoModel, Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "mobile_number", "contact_date")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "mobile_number")
+    list_per_page = 25
+
+
+admin.site.register(OwnerInfoModel)
+admin.site.register(Contact, ContactAdmin)
