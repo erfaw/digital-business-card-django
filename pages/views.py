d519b648a02c2d6af98352e8092936244400f2a6
erfaw <erfawn.h@gmail.com>
2026-07-29 18:48:15 +0330

    feat: fetch last record and send in context to index.html
    -docs: insert a TODO for further development.

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from pages.models import OwnerInfoModel
 
+
 def index(request):
     """
     Render actual digital business card.
     """
-    return render(request, "index.html")
+    all_records = OwnerInfoModel.objects.all().order_by("id")
+    last_record = all_records.last() # TODO get record of loggen in user.
+    context = {"owner": last_record}
+    return render(request, "index.html", context=context)
+
 
 def dashboard(request):
     """
@@ -13,21 +18,21 @@ def dashboard(request):
     """
     if request.method == "POST":
         new_owner = OwnerInfoModel.objects.create(
-            name=request.POST['name'],
-            role=request.POST['role'],
-            description=request.POST['description'],
-            logo_sub=request.POST['logo_sub'],
-            tag_behind=request.POST['tag_behind'],
-            email=request.POST['email'],
-            mobile_number=request.POST['mobile_number'],
-            website=request.POST['website'],
-            website_preview=request.POST['website_preview'],
-            linkedin=request.POST['linkedin'],
-            linkedin_preview=request.POST['linkedin_preview'],
-            location=request.POST['location'],
-            response_time=request.POST['response_time'],
+            name=request.POST["name"],
+            role=request.POST["role"],
+            description=request.POST["description"],
+            logo_sub=request.POST["logo_sub"],
+            tag_behind=request.POST["tag_behind"],
+            email=request.POST["email"],
+            mobile_number=request.POST["mobile_number"],
+            website=request.POST["website"],
+            website_preview=request.POST["website_preview"],
+            linkedin=request.POST["linkedin"],
+            linkedin_preview=request.POST["linkedin_preview"],
+            location=request.POST["location"],
+            response_time=request.POST["response_time"],
         )
         new_owner.save()
-        return redirect('index')
+        return redirect("index")
 
     return render(request, "dashboard.html")
