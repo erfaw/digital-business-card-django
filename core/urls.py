3106332703f31aa0d7d059259d1505eb007a8ccc
erfaw <erfawn.h@gmail.com>
2026-07-30 18:48:48 +0330

    feat: config MEDIA_URL and MEDIAR_ROOT if settings.base.DEBUG was True

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from core.settings import base as base_setting
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
 ]
+
+if base_setting.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
