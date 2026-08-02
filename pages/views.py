1e9d32b7e4b753274a3b941077e49bfffaf6ab9a
erfaw <erfawn.h@gmail.com>
2026-08-02 22:22:07 +0330

    refactor: import reverse from django.urls

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.urls import reverse
 from qrcode import QRCode, constants
 from io import BytesIO
 from pages.models import Contact
