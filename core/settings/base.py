4e3f692691ca5516041888a0731983c89618062a
erfaw <erfawn.h@gmail.com>
2026-08-03 05:56:40 +0330

    feat: config settings base, local, production for email
    -read credentials from os.environ

--- core/settings/base.py
+++ core/settings/base.py
@@ -78,3 +78,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- core/settings/production.py
+++ core/settings/production.py
@@ -7,3 +7,7 @@ load_dotenv()
 ALLOWED_HOSTS = ["*"]  # TODO (LOW) (PRODUCT) make sure about domain.
 
 SECRET_KEY = os.environ.get("LOCAL_SECRET_KEY")
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER_PRODUCTION")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD_PRODUCTION")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
