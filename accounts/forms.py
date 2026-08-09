e662bace6c8e60b0027d6feb06bae2cd77864c23
erfaw <erfawn.h@gmail.com>
2026-08-09 04:17:22 +0330

    feat: make RegisterForm()

--- accounts/forms.py
+++ accounts/forms.py
@@ -30,3 +30,77 @@ class LoginForm(forms.Form): # TODO (MID) refactor this with AuthenticationForm
             }
         ),
     )
+
+
+class RegisterForm(forms.Form): # TODO (MID) refactor this form with built-in for register forms in django. 
+    full_name = forms.CharField(
+        min_length=3,
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={
+                "class": "cf-input",
+                "type": "text",
+                "id": "cfName",
+                "name": "full_name",
+                "data-i18n-ph": "cf_name_ph", 
+                "autocomplete": "name",
+                "required": True,
+            }
+        ),
+    )
+    username = forms.CharField(
+        min_length=4,
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={
+                "class": "cf-input",
+                "type": "text",
+                "id": "cfName",
+                "name": "username",
+                "data-i18n-ph": "cf_name_ph",
+                "autocomplete": "username",
+                "required": True,
+            }
+        ),
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "cf-input", 
+                "type": "email", 
+                "id": "cfName", 
+                "name": "email", 
+                "data-i18n-ph": "cf_name_ph", 
+                "autocomplete": "email",
+                "required": True,
+            }
+        ),
+    )
+    password = forms.CharField(
+        min_length=8,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "cf-input", 
+                "type": "password", 
+                "id": "cfName", 
+                "name": "password", 
+                "data-i18n-ph": "cf_name_ph", 
+                "autocomplete": "new-password", 
+                "required": True,
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        min_length=8,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "cf-input", 
+                "type": "password", 
+                "id": "cfName", 
+                "name": "password2", 
+                "data-i18n-ph": "cf_name_ph", 
+                "autocomplete": "new-password", 
+                "required": True,
+            }
+        ),
+    )
