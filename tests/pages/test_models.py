d5ba4f2fd760e1dc3e660395709518b2526d65f5
erfaw <erfawn.h@gmail.com>
2026-08-11 08:58:15 +0330

    test: BusinessCardModel.test_name_field_maximum_length()

--- tests/pages/test_models.py
+++ tests/pages/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 
 pytestmark = pytest.mark.django_db
@@ -8,3 +9,13 @@ class TestBusinessCardModel:
     def test_dunder_str(self, business_card_factory):
         unit = business_card_factory()
         assert unit.__str__() == unit.name
+
+    def test_name_field_maximum_length(self, business_card_factory):
+        MAX_LENGTH = 50
+        obj = business_card_factory(
+            name=(MAX_LENGTH+1)*'a'
+        )
+        with pytest.raises(ValidationError):
+            obj.full_clean()
+        
+
