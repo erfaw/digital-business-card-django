0c85a067992aff30bb9b5497e2a33f0955f13ace
erfaw <erfawn.h@gmail.com>
2026-08-11 15:26:30 +0330

    test: TestPagesViews.test_public_card_view_new_contact_on_post()

--- tests/pages/test_views.py
+++ tests/pages/test_views.py
@@ -1,4 +1,8 @@
+import pytest
 from django.test import Client
+
+
+pytestmark = pytest.mark.django_db
 
 
 class TestPagesViews:
@@ -11,3 +15,17 @@ class TestPagesViews:
         client = Client()
         response = client.get("/")
         assert "pages/index.html" in [t.name for t in response.templates]
+
+    def test_public_card_view_new_contact_on_post(self, user_factory, business_card_factory):
+        user = user_factory()
+        bc = business_card_factory(user=user)
+        client = Client()
+        params = {
+            "name": "test-name",
+            "mobile_number": 20*"1",
+            "message": "test_message"
+        }
+        response = client.post(f"/u/{user.username}/", params)
+        assert response.status_code == 302
+        response = client.post(f"/u/{user.username}/", params, follow=True)
+        assert response.status_code == 200
