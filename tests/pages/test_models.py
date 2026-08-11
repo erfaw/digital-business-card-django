ac750a1cb96a7dcddf63f677472a64b94e561aba
erfaw <erfawn.h@gmail.com>
2026-08-11 09:31:17 +0330

    test: TestBusinessCardModel.test_response_time_field_blank_true()

--- tests/pages/test_models.py
+++ tests/pages/test_models.py
@@ -93,3 +93,8 @@ class TestBusinessCardModel:
         obj = business_card_factory()
         obj.full_clean()
         assert obj.location == ""
+
+    def test_response_time_field_blank_true(self, business_card_factory):
+        obj = business_card_factory()
+        obj.full_clean()
+        assert obj.response_time == ""
