2bd1d37727b89fca3307ce93222236f1471dcde1
erfaw <erfawn.h@gmail.com>
2026-08-11 09:23:34 +0330

    test: TestBusinessCardModel.test_website_field_blank_true()

--- tests/pages/test_models.py
+++ tests/pages/test_models.py
@@ -54,3 +54,8 @@ class TestBusinessCardModel:
         )
         with pytest.raises(ValidationError):
             obj.full_clean()
+
+    def test_website_field_blank_true(self, business_card_factory):
+        obj = business_card_factory()
+        obj.full_clean()
+        assert obj.website == ""
