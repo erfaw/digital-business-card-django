98fa75e5b0bc2cd093815b5c8d664b31ef45e692
erfaw <erfawn.h@gmail.com>
2026-08-11 07:14:43 +0330

    feat: make UserFactory() and add it as subfactory to BusinessCardFactory()

--- tests/factories.py
+++ tests/factories.py
@@ -1,5 +1,12 @@
 import factory
+from factory.declarations import SubFactory
 from pages.models import BusinessCard
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
 
 
 class BusinessCardFactory(factory.django.DjangoModelFactory):
@@ -7,3 +14,4 @@ class BusinessCardFactory(factory.django.DjangoModelFactory):
         model = BusinessCard
 
     name = "test_name"
+    user = SubFactory(UserFactory)
