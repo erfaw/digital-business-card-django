f1edb5807749e8f2c36a67e26b1185cce86afee0
erfaw <erfawn.h@gmail.com>
2026-08-11 14:10:00 +0330

    test: make ContactFactory()

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,6 @@
 import factory
 from factory.declarations import SubFactory, Sequence
-from pages.models import BusinessCard
+from pages.models import BusinessCard, Contact
 from django.contrib.auth.models import User
 
 
@@ -22,4 +22,13 @@ class BusinessCardFactory(factory.django.DjangoModelFactory):
     tag_behind = "test_tag_behind"
     mobile_number = "+989000000000"
     email = Sequence(lambda n: f"test_{n}_user@gmail.com")
+    user = SubFactory(UserFactory)
+
+
+class ContactFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model=Contact
+
+    name = Sequence(lambda n: f"test_name_{n}")
+    mobile_number = Sequence(lambda n: f"test_mobile_number_{n}")
     user = SubFactory(UserFactory)
