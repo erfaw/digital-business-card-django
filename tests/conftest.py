b518ad041c31997a233e1df3aa84fe4738c3fddb
erfaw <erfawn.h@gmail.com>
2026-08-11 14:10:39 +0330

    test: register ContactFactory() with pytest_factoryboy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pytest_factoryboy import register
-from .factories import BusinessCardFactory, UserFactory
+from .factories import BusinessCardFactory, UserFactory, ContactFactory
 
 
 register(BusinessCardFactory)
 register(UserFactory)
+register(ContactFactory)
