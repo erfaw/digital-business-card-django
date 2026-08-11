5b71c42eda6274902aa5712e64cc735ab25710d9
erfaw <erfawn.h@gmail.com>
2026-08-11 07:19:24 +0330

    test: modify TestBusinessCardModel.test_dunder_str
    -to assert unit.name instead of hard coded name

--- tests/pages/test_models.py
+++ tests/pages/test_models.py
@@ -7,4 +7,4 @@ pytestmark = pytest.mark.django_db
 class TestBusinessCardModel:
     def test_dunder_str(self, business_card_factory):
         unit = business_card_factory()
-        assert unit.__str__() == "test_name"
+        assert unit.__str__() == unit.name
