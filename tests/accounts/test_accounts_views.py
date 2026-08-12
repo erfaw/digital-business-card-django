ab73f8942235824840cabc3ae3b2c2fb0ea90113
erfaw <erfawn.h@gmail.com>
2026-08-12 21:20:28 +0330

    test: TestAccountsForms.test_user_registration_on_post()

--- tests/accounts/test_accounts_views.py
+++ tests/accounts/test_accounts_views.py
@@ -11,6 +11,39 @@ class TestRegisterView:
         response = c.get(reverse("register"))
         assert response.status_code == 200
 
+    def test_user_registration_on_post(self, user_factory, business_card_factory):
+        user_class = user_factory().__class__
+        business_card_class = business_card_factory().__class__
+        c = Client()
+        params = {
+            "full_name": "test testi",
+            "username": "test_testi",
+            "email": "test@gmail.com",
+            "password": "1"*8,
+            "password2": "1"*8,
+        }
+        response = c.post(
+            reverse("register"), 
+            params,
+        )
+        assert response.status_code == 302
+        assert (
+            user_class.objects
+            .filter(username=params["username"])
+            .exists()
+        )
+        assert (
+            user_class.objects
+            .filter(username=params["username"])
+            .exists()
+        )
+        made_user = user_class.objects.get(username=params["username"])
+        assert not made_user == None
+        made_user.refresh_from_db()
+        assert isinstance(
+            made_user.business_card,
+            business_card_class
+        ) 
     # TODO (HIGH) write test for all aspects of register
     # TODO (HIGH) test for login
     # TODO (HIGH) test for logout
