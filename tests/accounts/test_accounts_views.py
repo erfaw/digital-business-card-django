124ed3d3ececfdfd9f401b46cf325ecf35d56ee3
erfaw <erfawn.h@gmail.com>
2026-08-12 20:45:05 +0330

    test: modify test class name and method name
    -change TestAccountsViews to TestRegisterView
    -remove '..._register_view_...' prefix from method names

--- tests/accounts/test_accounts_views.py
+++ tests/accounts/test_accounts_views.py
@@ -5,8 +5,8 @@ from django.test import Client
 pytestmark = pytest.mark.django_db
 
 
-class TestAccountsViews:
-    def test_register_view_status_code(self):
+class TestRegisterView:
+    def test_status_code(self):
         c = Client()
         response = c.get(reverse("register"))
         assert response.status_code == 200
