961b84e4012c991d37c55e0b32c5b332660aef7f
erfaw <erfawn.h@gmail.com>
2026-08-12 19:28:41 +0330

    test: TestAccountsForms.test_register_form_full_name_field_max_length()

--- tests/accounts/test_accounts_forms.py
+++ tests/accounts/test_accounts_forms.py
@@ -25,3 +25,11 @@ class TestAccountsForms:
         form = RegisterForm(td)
         with pytest.raises(AssertionError):
             assert form.is_valid()
+
+    def test_register_form_full_name_field_max_length(self):
+        MAX_LEN = 200
+        td = self.test_data
+        td["full_name"] = "s" * (MAX_LEN + 1)
+        form = RegisterForm(td)
+        with pytest.raises(AssertionError):
+            assert form.is_valid()
