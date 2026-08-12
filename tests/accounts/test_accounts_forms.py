f4a9f13f2d0fd646aa93aa4f3a6ff222da8d417a
erfaw <erfawn.h@gmail.com>
2026-08-12 19:35:06 +0330

    test: make copy of test_data class attribute

--- tests/accounts/test_accounts_forms.py
+++ tests/accounts/test_accounts_forms.py
@@ -14,13 +14,13 @@ class TestAccountsForms:
     }
 
     def test_register_form_is_valid(self):
-        td = self.test_data
+        td = self.test_data.copy()
         form = RegisterForm(td)
         assert form.is_valid()
 
     def test_register_form_full_name_field_min_length(self):
         MIN_LEN = 3
-        td = self.test_data
+        td = self.test_data.copy()
         td["full_name"] = "s" * (MIN_LEN - 1)
         form = RegisterForm(td)
         with pytest.raises(AssertionError):
@@ -28,7 +28,7 @@ class TestAccountsForms:
 
     def test_register_form_full_name_field_max_length(self):
         MAX_LEN = 200
-        td = self.test_data
+        td = self.test_data.copy()
         td["full_name"] = "s" * (MAX_LEN + 1)
         form = RegisterForm(td)
         with pytest.raises(AssertionError):
@@ -36,7 +36,7 @@ class TestAccountsForms:
 
     def test_register_form_username_field_min_length(self):
         MIN_LEN = 4
-        td = self.test_data
+        td = self.test_data.copy()
         td["username"] = "s" * (MIN_LEN - 1)
         form = RegisterForm(td)
         with pytest.raises(AssertionError):
@@ -44,7 +44,7 @@ class TestAccountsForms:
 
     def test_register_form_username_field_max_length(self):
         MAX_LEN = 200
-        td = self.test_data
+        td = self.test_data.copy()
         td["username"] = "s" * (MAX_LEN + 1)
         form = RegisterForm(td)
         with pytest.raises(AssertionError):
