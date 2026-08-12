efe0e7d21f76d0a5967c5bd634fbe50d451d0692
erfaw <erfawn.h@gmail.com>
2026-08-12 19:27:09 +0330

    test: TestAccountsForms.test_register_form_full_name_field_min_length()
    -also add test_data as class attribute to use it easier.
    -modify previous test with this change.

--- tests/accounts/test_accounts_forms.py
+++ tests/accounts/test_accounts_forms.py
@@ -5,13 +5,23 @@ pytestmark = pytest.mark.django_db
 
 
 class TestAccountsForms:
-    def test_register_form_is_valid(self, login_form):
-        test_data = {
-            "full_name": "test testi",
-            "username": "test_testi",
-            "email": "test@gmail.com",
-            "password": "123456789",
-            "password2": "123456789",
-        }
-        form = RegisterForm(test_data)
+    test_data = {
+        "full_name": "test testi",
+        "username": "test_testi",
+        "email": "test@gmail.com",
+        "password": "123456789",
+        "password2": "123456789",
+    }
+
+    def test_register_form_is_valid(self):
+        td = self.test_data
+        form = RegisterForm(td)
         assert form.is_valid()
+
+    def test_register_form_full_name_field_min_length(self):
+        MIN_LEN = 3
+        td = self.test_data
+        td["full_name"] = "s" * (MIN_LEN - 1)
+        form = RegisterForm(td)
+        with pytest.raises(AssertionError):
+            assert form.is_valid()
