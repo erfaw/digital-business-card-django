3a3dacc03e0863a78e8618d1a766c25ba6712110
erfaw <erfawn.h@gmail.com>
2026-08-14 20:41:20 +0330

    feat: add DATABASES \w postgres configurations on docker compose

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,6 +6,18 @@ load_dotenv()
 
 ALLOWED_HOSTS = ["*"]  # TODO (LOW) (PRODUCT) make sure about domain.
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "digital_business_card_django",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "database",
+        "PORT": "5432",
+    }
+}
+"""Database configurations with Docker service postgres with volume."""
+
 SECRET_KEY = os.environ.get("PRODUCTION_SECRET_KEY")
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER_PRODUCTION")
